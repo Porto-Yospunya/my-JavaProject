53ba3dc405c40d04449c674f19f2fc3792a6e1d5
Porto <watcharapong16599@gmail.com>
2024-02-24 03:54:39 +0700

    Update file and Add file

--- src/Home.java
+++ src/Home.java
@@ -1,41 +1,54 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
 public class Home extends JFrame
 {
     // Panel variable setup.
-    private JPanel bgPanel1;
+    private JPanel titlebar;
     private JPanel bgPanel2;
-    private JPanel tabPanel1;
-    private JPanel tabPanel2;
+    private JPanel tab1;
+    private JPanel tab2;
+    private JPanel tab3;
     
     // Label variable setup.
     private JLabel exitLabelButton;
     private JLabel minimizeLabelButton;
     private JLabel titLabel;
-    private JLabel menuLabel1;
-    private JLabel menuLabel2;
+    private JLabel textmenuLabel1;
+    private JLabel textmenuLabel2;
+    private JLabel menuLabelButton1;
+    private JLabel menuLabelButton2;
+    private JLabel menuLabelButton3;
+
+    private JLabel textweiLabel;
+    private JLabel textheiLabel;
+    private JLabel textweiunitLabel;
+    private JLabel textheiunitLabel;
 
     // Button variable setup.
-    private JButton menuButton1;
-    private JButton menuButton2;
+    private JButton calculButton;
+
+    // Text Field variable setup.
+    private JTextField weiTextInput;
+    private JTextField heiTextInput;
 
     // Separator variable setup.
     private JSeparator titSeparator;
+    private JSeparator menuSeparator;
 
-    private JTabbedPane jTab;
+    private JTabbedPane jTabbed;
 
     // Show display
     public Home()
     {
         setup();
-        titlePanel(bgPanel1, exitLabelButton, minimizeLabelButton, titLabel, titSeparator);
-        formBackgroundPanel(menuLabel1, menuLabel2);
-        menuPanel(bgPanel2);
+        titlePanel();
+        tabPanel();
+        menuPanel();
     }
 
     // Setup display.
@@ -46,175 +59,270 @@ public class Home extends JFrame
         setSize(new Dimension(1200, 800));
         setResizable(false);
         setLayout(null);
-        setLocation(400, 100);
-        setTitle("Clock Store");
+        setLocationRelativeTo(null);
     }
 
     // Title Panel setup.
-    private void titlePanel(JPanel jPanel, JLabel jLabel1, JLabel jLabel2, JLabel jLabel3, JSeparator jSeparator)
+    private void titlePanel()
     {
-        jPanel = new JPanel();
-        jPanel.setBounds(0, 0, 1200, 40);
-        jPanel.setBackground(new Color(51, 51, 51));
-        jPanel.setLayout(null);
-        add(jPanel);
+        titlebar = new JPanel();
+        titlebar.setBounds(0, 0, 1200, 40);
+        titlebar.setBackground(new Color(0, 100, 0));
+        titlebar.setLayout(null);
+        add(titlebar);
 
-        jLabel1 = new JLabel();
-        jLabel1.setText("x");
-        jLabel1.setForeground(new Color(255, 87, 51));
-        jLabel1.setFont(new Font("", Font.BOLD, 30));
-        jLabel1.setBounds(1165, -3, 50, 40);
-        jLabel1.addMouseListener(new MouseAdapter() 
+        exitLabelButton = new JLabel();
+        exitLabelButton.setText("x");
+        exitLabelButton.setForeground(new Color(255, 255, 255));
+        exitLabelButton.setFont(new Font("", Font.BOLD, 30));
+        exitLabelButton.setBounds(1165, -3, 50, 40);
+        exitLabelButton.addMouseListener(new MouseAdapter() 
         {
             public void mouseClicked(MouseEvent evt)
             {
                 exitLabelButtonPerformed(evt);
             }    
-        });
-        add(jLabel1);
 
-        jLabel2 = new JLabel();
-        jLabel2.setText("_");
-        jLabel2.setForeground(new Color(255, 87, 51));
-        jLabel2.setFont(new Font("", Font.BOLD, 30));
-        jLabel2.setBounds(1130, -15, 50, 40);
-        jLabel2.addMouseListener(new MouseAdapter()
+            public void mouseEntered(MouseEvent evt)
+            {
+                exitLabelButton.setForeground(new Color(250, 50, 10));
+            }
+
+            public void mouseExited(MouseEvent evt)
+            {
+                exitLabelButton.setForeground(new Color(255, 255, 255));
+            }
+        });
+
+        minimizeLabelButton = new JLabel();
+        minimizeLabelButton.setText("_");
+        minimizeLabelButton.setForeground(new Color(255, 255, 255));
+        minimizeLabelButton.setFont(new Font("", Font.BOLD, 30));
+        minimizeLabelButton.setBounds(1130, -15, 50, 40);
+        minimizeLabelButton.addMouseListener(new MouseAdapter()
         {
             public void mouseClicked(MouseEvent evt)
             {
                 minimizeLabelButtonPerformed(evt);
-            }    
+            }
         });
-        add(jLabel2);
 
-        jSeparator = new JSeparator();
-        jSeparator.setBackground(new Color(255, 95, 31));
-        jSeparator.setForeground(new Color(255, 95, 31));
-        jSeparator.setBounds(0, 40, 1200, 20);
-        add(jSeparator);
+        titSeparator = new JSeparator();
+        titSeparator.setBackground(new Color(255, 255, 255));
+        titSeparator.setForeground(null);
+        titSeparator.setBounds(0, 40, 1200, 4);
+        titSeparator.setOpaque(true);
+        //add(jSeparator);
 
-        jLabel3 = new JLabel();
-        jLabel3.setText("Unit Conversion");
-        jLabel3.setForeground(new Color(255, 87, 51));
-        jLabel3.setFont(new Font("", Font.BOLD, 26));
-        jLabel3.setBounds(10, 0, 250, 40);
+        titLabel = new JLabel();
+        titLabel.setText("GOP GRAB");
+        titLabel.setForeground(new Color(255, 255, 255));
+        titLabel.setFont(new Font("", Font.BOLD, 26));
+        titLabel.setBounds(10, 0, 250, 40);
 
-        jPanel.add(jLabel1);
-        jPanel.add(jLabel2);
-        jPanel.add(jLabel3);
+        titlebar.add(exitLabelButton);
+        titlebar.add(minimizeLabelButton);
+        titlebar.add(titLabel);
     }
 
     // Form Background Panel setup.
-    private void formBackgroundPanel(JLabel jLabel1, JLabel jLabel2)
+    private void tabPanel()
     {
-        jTab = new JTabbedPane();
-        jTab.setBounds(250, 40, 1002, 760);
-        jTab.setTabLayoutPolicy(JTabbedPane.SCROLL_TAB_LAYOUT);
-        jTab.setTabPlacement(JTabbedPane.RIGHT);
+        jTabbed = new JTabbedPane();
+        jTabbed.setBounds(250, 36, 1004, 767);
+        jTabbed.setTabPlacement(JTabbedPane.RIGHT);
 
-        tabPanel1 = new JPanel();
-        tabPanel1.setBounds(250, 40, 950, 760);
-        tabPanel1.setBackground(new Color(61, 61 ,61));
-        tabPanel1.setLayout(null);
-        add(tabPanel1);
+        tab1 = new JPanel();
+        tab1.setBounds(250, 40, 950, 760);
+        tab1.setBackground(new Color(255, 255, 255));
+        tab1.setLayout(null);
 
-        jLabel1 = new JLabel();
-        jLabel1.setText("Welcome");
-        jLabel1.setBounds(20, 5, 200, 50);
-        jLabel1.setFont(new Font("", Font.PLAIN, 20));
-        jLabel1.setForeground(new Color(255, 255, 255));
-        add(jLabel1);
+        textmenuLabel1 = new JLabel();
+        textmenuLabel1.setText("Welcome");
+        textmenuLabel1.setBounds(20, 5, 200, 50);
+        textmenuLabel1.setFont(new Font("", Font.PLAIN, 20));
+        textmenuLabel1.setForeground(new Color(0, 0, 0));
+        tab1.add(textmenuLabel1);
 
-        tabPanel1.add(jLabel1);
+        tab2 = new JPanel();
+        tab2.setBounds(250, 40, 950, 760);
+        tab2.setBackground(new Color(255, 255, 255));
+        tab2.setLayout(null);
 
-        tabPanel2 = new JPanel();
-        tabPanel2.setBounds(250, 40, 950, 760);
-        tabPanel2.setBackground(new Color(61, 61 ,61));
-        tabPanel2.setLayout(null);
-        add(tabPanel2);
+        textmenuLabel2 = new JLabel();
+        textmenuLabel2.setText("Hello");
+        textmenuLabel2.setBounds(20, 5, 200, 50);
+        textmenuLabel2.setFont(new Font("", Font.PLAIN, 20));
+        textmenuLabel2.setForeground(new Color(0, 0, 0));
+        tab2.add(textmenuLabel2);
 
-        jLabel2 = new JLabel();
-        jLabel2.setText("Hello");
-        jLabel2.setBounds(20, 5, 200, 50);
-        jLabel2.setFont(new Font("", Font.PLAIN, 20));
-        jLabel2.setForeground(new Color(255, 255, 255));
-        add(jLabel2);
+        tab3 = new JPanel();
+        tab3.setBounds(250, 40, 950, 760);
+        tab3.setBackground(new Color(255, 255, 255));
+        tab3.setLayout(null);
+        calculBMI();
 
-        tabPanel2.add(jLabel2);
-
-        jTab.addTab("Tab1", tabPanel1);
-        jTab.addTab("Tab2", tabPanel2);
-
-        add(jTab);
+        jTabbed.addTab("1", tab1);
+        jTabbed.addTab("2", tab2);
+        jTabbed.addTab("3", tab3);
+        add(jTabbed);
     }
 
     // Menu Panel setup.
-    private void menuPanel(JPanel jPanel)
+    private void menuPanel()
     {
-        jPanel = new JPanel();
-        jPanel.setBounds(0, 40, 250, 760);
-        jPanel.setBackground(new Color(200, 200 ,200));
-        jPanel.setLayout(null);
-        add(jPanel);
+        bgPanel2 = new JPanel();
+        bgPanel2.setBounds(0, 40, 250, 760);
+        bgPanel2.setBackground(new Color(255, 255, 255));
+        bgPanel2.setLayout(null);
+        add(bgPanel2);
 
-        menuButton1 = new JButton();
-        menuButton1.setText("SI base units");
-        menuButton1.setBackground(new Color(200, 200, 200));
-        menuButton1.setFont(new Font("", Font.BOLD, 25));
-        menuButton1.setBounds(20, 10, 200, 40);
-        menuButton1.setBorder(null);
-        menuButton1.setBorderPainted(false);
-        menuButton1.addActionListener(new ActionListener() 
+        menuLabelButton1 = new JLabel();
+        menuLabelButton1.setText("HOME");
+        menuLabelButton1.setForeground(new Color(0, 0, 0));
+        menuLabelButton1.setFont(new Font("", Font.BOLD, 25));
+        menuLabelButton1.setBounds(20, 10, 200, 40);
+        menuLabelButton1.setHorizontalAlignment(JLabel.LEFT);
+        menuLabelButton1.addMouseListener(new MouseAdapter() 
         {
-            public void actionPerformed(ActionEvent evt)
+            public void mouseClicked(MouseEvent evt)
             {
-                menuButton1Performed(evt);
+                menuLabelButton1Performed(evt);
             }
-        });
-        menuButton1.addMouseListener(new MouseAdapter() 
-        {
+
             public void mouseEntered(MouseEvent evt)
             {
-                menuButton1.setForeground(new Color(255, 87, 51));
+                menuLabelButton1.setForeground(new Color(0, 100, 0));
             }
         
             public void mouseExited(MouseEvent evt)
             {
-                menuButton1.setForeground(new Color(0, 0, 0));
+                menuLabelButton1.setForeground(new Color(0, 0, 0));
             }
         });
-        add(menuButton1);
 
-        menuButton2 = new JButton();
-        menuButton2.setText("Derived SI units");
-        menuButton2.setBackground(new Color(200, 200, 200));
-        menuButton2.setFont(new Font("", Font.BOLD, 25));
-        menuButton2.setBounds(20, 60, 200, 40);
-        menuButton2.setBorder(null);
-        menuButton2.setBorderPainted(false);
-        menuButton2.addActionListener(new ActionListener() 
+        menuLabelButton2 = new JLabel();
+        menuLabelButton2.setText("MENU");
+        menuLabelButton2.setForeground(new Color(0, 0, 0));
+        menuLabelButton2.setFont(new Font("", Font.BOLD, 25));
+        menuLabelButton2.setBounds(20, 60, 200, 40);
+        menuLabelButton2.setHorizontalAlignment(JLabel.LEFT);
+        menuLabelButton2.addMouseListener(new MouseAdapter()
         {
-            public void actionPerformed(ActionEvent evt)
+            public void mouseClicked(MouseEvent evt)
             {
-                menuButton2Performed(evt);
+                menuLabelButton2Performed(evt);
             }
-        });
-        menuButton2.addMouseListener(new MouseAdapter() 
-        {
+            
             public void mouseEntered(MouseEvent evt)
             {
-                menuButton2.setForeground(new Color(255, 87, 51));
+                menuLabelButton2.setForeground(new Color(0, 100, 0));
             }
         
             public void mouseExited(MouseEvent evt)
             {
-                menuButton2.setForeground(new Color(0, 0, 0));
+                menuLabelButton2.setForeground(new Color(0, 0, 0));
             }
         });
-        add(menuButton2);
 
-        jPanel.add(menuButton1);
-        jPanel.add(menuButton2);
+        menuLabelButton3 = new JLabel();
+        menuLabelButton3.setText("BMI");
+        menuLabelButton3.setForeground(new Color(0, 0, 0));
+        menuLabelButton3.setFont(new Font("", Font.BOLD, 25));
+        menuLabelButton3.setBounds(20, 110, 200, 40);
+        menuLabelButton3.setHorizontalAlignment(JLabel.LEFT);
+        menuLabelButton3.addMouseListener(new MouseAdapter()
+        {
+            public void mouseClicked(MouseEvent evt)
+            {
+                menuLabelButton3Performed(evt);
+            }
+            
+            public void mouseEntered(MouseEvent evt)
+            {
+                menuLabelButton3.setForeground(new Color(0, 100, 0));
+            }
+        
+            public void mouseExited(MouseEvent evt)
+            {
+                menuLabelButton3.setForeground(new Color(0, 0, 0));
+            }
+        });
+
+        menuSeparator = new JSeparator();
+        menuSeparator.setBackground(new Color(0, 100, 0));
+        menuSeparator.setForeground(new Color(0, 100, 0));
+        menuSeparator.setBounds(240, 0, 10, 760);
+        menuSeparator.setOpaque(true);
+
+        bgPanel2.add(menuLabelButton1);
+        bgPanel2.add(menuLabelButton2);
+        bgPanel2.add(menuLabelButton3);
+        bgPanel2.add(menuSeparator);
+    }
+
+    private void calculBMI()
+    {
+        textweiLabel = new JLabel();
+        textweiLabel.setText("Weight:");
+        textweiLabel.setFont(new Font("", Font.BOLD, 30));
+        textweiLabel.setForeground(new Color(0, 0, 0));
+        textweiLabel.setBounds(40, 50, 120, 40);
+        textweiLabel.setHorizontalAlignment(JLabel.LEFT);
+
+        textweiunitLabel = new JLabel();
+        textweiunitLabel.setText("kg.");
+        textweiunitLabel.setFont(new Font("", Font.BOLD, 30));
+        textweiunitLabel.setForeground(new Color(0, 0, 0));
+        textweiunitLabel.setBounds(680, 50, 120, 40);
+        textweiunitLabel.setHorizontalAlignment(JLabel.LEFT);
+
+        textheiLabel = new JLabel();
+        textheiLabel.setText("Height:");
+        textheiLabel.setFont(new Font("", Font.BOLD, 30));
+        textheiLabel.setForeground(new Color(0, 0, 0));
+        textheiLabel.setBounds(40, 150, 120, 40);
+        textheiLabel.setHorizontalAlignment(JLabel.LEFT);
+
+        textheiunitLabel = new JLabel();
+        textheiunitLabel.setText("cm.");
+        textheiunitLabel.setFont(new Font("", Font.BOLD, 30));
+        textheiunitLabel.setForeground(new Color(0, 0, 0));
+        textheiunitLabel.setBounds(680, 150, 120, 40);
+        textheiunitLabel.setHorizontalAlignment(JLabel.LEFT);
+
+        weiTextInput = new JTextField();
+        weiTextInput.setBorder(BorderFactory.createMatteBorder(0, 0, 2, 0, new Color(0, 0, 0)));
+        weiTextInput.setFont(new Font("", Font.PLAIN, 30));
+        weiTextInput.setBounds(160, 50, 500, 40);
+
+        heiTextInput = new JTextField();
+        heiTextInput.setBorder(BorderFactory.createMatteBorder(0, 0, 2, 0, new Color(0, 0, 0)));
+        heiTextInput.setFont(new Font("", Font.PLAIN, 30));
+        heiTextInput.setBounds(160, 150, 500, 40);
+
+        calculButton = new JButton();
+        calculButton.setText("Enter");
+        calculButton.setFont(new Font("", Font.PLAIN, 20));
+        calculButton.setBorder(BorderFactory.createLineBorder(new Color(0, 0, 0), 2));
+        calculButton.setBackground(null);
+        calculButton.setForeground(new Color(0, 0, 0));
+        calculButton.setBounds(40, 250, 100, 40);
+        calculButton.addActionListener(new ActionListener() 
+        {
+            public void actionPerformed(ActionEvent evt)
+            {
+                calaulSystem(evt);
+            }    
+        });
+
+        tab3.add(textweiLabel);
+        tab3.add(textheiLabel);
+        tab3.add(textweiunitLabel);
+        tab3.add(textheiunitLabel);
+        tab3.add(weiTextInput);
+        tab3.add(heiTextInput);
+        tab3.add(calculButton);
     }
 
     // Exit Button setup.
@@ -229,14 +337,24 @@ public class Home extends JFrame
         setState(Frame.ICONIFIED);
     }
 
-    private void menuButton1Performed(ActionEvent evt)
+    private void menuLabelButton1Performed(MouseEvent evt)
     {
-        jTab.setSelectedIndex(0);
+        jTabbed.setSelectedIndex(0);
     }
 
-    private void menuButton2Performed(ActionEvent evt)
+    private void menuLabelButton2Performed(MouseEvent evt)
     {
-        jTab.setSelectedIndex(1);
+        jTabbed.setSelectedIndex(1);
+    }
+
+    private void menuLabelButton3Performed(MouseEvent evt)
+    {
+        jTabbed.setSelectedIndex(2);
+    }
+
+    private void calaulSystem(ActionEvent evt)
+    {
+
     }
 
     // Main Run program.
