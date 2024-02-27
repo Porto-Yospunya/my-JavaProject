9f2292f6966cf9bf0a1b2ee52cd9db9683c4fe09
Porto <watcharapong16599@gmail.com>
2024-02-27 23:40:25 +0700

    Update file Calculator

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,9 +1,22 @@
 public class Calculator
 {
+    // Calculator BMI value.
     public static String calculatorBMI(double wei, double hei)
     {
-        //Calculator BMI value.
         double bmi = wei / (Math.pow((hei / 100), 2));
         return String.format("%.1f", bmi);
     }
+/* 
+
+    // Calculator BMR Male value.
+    public static String calculatorBMR(double wei, double hei, int age, String gender)
+    {
+        if (gender.equals("male")) {
+            double BMRmale = (665 + (9.6 * wei) + (1.8 * hei) - (4.7 * age)) /3;
+            return String.format("%.1f", BMRmale);
+        } else if (gender.equals("female")) {
+            double BMRFemale = (665 + (9.6 * wei) + (1.8 * hei) - (4.7 * age)) /3;
+            return String.format("%.1f", BMRFemale);
+        }
+    }*/
 }
